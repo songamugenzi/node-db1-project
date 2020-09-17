04104cd7708ea8cdf493fefe60d9eba09c03d5f5
S Mugenzi <smugenzi@Ss-MacBook-Air.local>
2020-09-17 17:46:00 -0400

    Set up server.js to use accountsRouter

--- api/server.js
+++ api/server.js
@@ -1,9 +1,15 @@
 const express = require("express");
 
 const db = require("../data/dbConfig.js");
+const accountsRouter = require("../accounts/accounts-router.js");
 
 const server = express();
 
 server.use(express.json());
+server.use("/api/accounts", accountsRouter);
+
+server.get("/", (req, res) => {
+  res.status(200).json({ api: "up" });
+});
 
 module.exports = server;
